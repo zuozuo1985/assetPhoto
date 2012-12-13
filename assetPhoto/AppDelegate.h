17f381570095b0e4a7f56b04cb0e58f26552ee14
user <qizuo.zhu@gmail.com>
2012-12-13 10:36:06 +0800

    Initial Commit

---
+++ assetPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  assetPhoto
+//
+//  Created by user on 12-12-13.
+//  Copyright (c) 2012年 Eastech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ assetPhoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  assetPhoto
+//
+//  Created by user on 12-12-13.
+//  Copyright (c) 2012年 Eastech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
