3614537c2509c9e20dc1cd3496bb5343fb2a13ad
zuozuo1985 <qizuo.zhu@gmail.com>
2013-05-09 15:03:38 +0800

    first 20130509
    cssxxdx

---
+++ assetViewController.h
@@ -0,0 +1,15 @@
+//
+//  assetViewController.h
+//  assetPhoto
+//
+//  Created by user on 12-12-13.
+//  Copyright (c) 2012年 Eastech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface assetViewController : UITableViewController
+
+
+@property(nonatomic,retain)NSMutableArray *datasource;
+@end
---
+++ assetViewController.m
@@ -0,0 +1,154 @@
+//
+//  assetViewController.m
+//  assetPhoto
+//
+//  Created by user on 12-12-13.
+//  Copyright (c) 2012年 Eastech. All rights reserved.
+//
+
+#import "assetViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "PhotoAssets.h"
+
+@interface assetViewController ()
+
+@end
+
+#define kAssetsImageViewTag 100
+@implementation assetViewController
+@synthesize datasource;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    datasource = [[NSMutableArray alloc] init];
+    
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+        if (group) {
+            [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+            [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+                if (result) {
+                    //因为alasset循环结束就会实效，所有要取出属性封装成自己的model
+                    PhotoAssets *photoAssets = [[PhotoAssets alloc]init];
+                    //小图
+                    photoAssets.thumnail = [UIImage imageWithCGImage:result.thumbnail];
+                    //全图
+                    photoAssets.fullImage = [UIImage imageWithCGImage:result.defaultRepresentation.fullResolutionImage];
+                    
+                    [datasource addObject:photoAssets];
+                    NSLog(@"datasource====%@",datasource);
+                }
+            }];
+        } else {
+            [self.tableView reloadData];
+        }
+    } failureBlock:^(NSError *error) {
+        NSLog(@"Failed.");
+    }];
+
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return datasource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    PhotoAssets *photoAsset = [datasource objectAtIndex:indexPath.row];
+    cell.imageView.image = photoAsset.thumnail;
+   // cell.textLabel.text = photoAlbum.albumName;
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
